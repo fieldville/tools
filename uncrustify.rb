1020052bd88e74372c7ade70896b12e03ac86c17
naoki_ichihara <naoki_ichihara@sobal.co.jp>
2014-03-31 09:28:49 +0900

    Modify config path setting logic

--- uncrustify.rb
+++ uncrustify.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 base_path = ENV['PWD']
+config_path = "#{base_path}/.uncrustifyconfig"
+config_path = "#{ENV['HOME']}/.uncrustify/uncrustify.cfg" if !File.exist?(config_path)
 
 puts "running uncrustify for xcode project path: #{base_path}"
+puts "                                   config: #{config_path}"
 
 if base_path != nil
   paths = `find "#{base_path}" -name "*.m" -o -name "*.h" -o -name "*.mm" -o -name "*.c"`
@@ -13,7 +16,7 @@ if base_path != nil
     path.gsub(/(^[^\n]+?)(\n)/, '"\\1"')
   end
   paths = paths.join(" ")
-  result = `/usr/local/bin/uncrustify -c ~/.dotfiles/.uncrustify.cfg --no-backup #{paths}`;
+  result = `/usr/local/bin/uncrustify -c "#{config_path}" --no-backup #{paths}`;
   puts result
 else
   puts "Invalid base path..."
